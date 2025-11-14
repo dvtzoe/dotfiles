3367e0029f85e5f650175a0edada6332958a6dfa
dvtzoe <ohmtzoe@gmail.com>
2025-11-14 19:23:15 +0700

    add ~/.config/nvim/

---
+++ .config/nvim/lua/community.lua
@@ -0,0 +1,24 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.fuzzy-finder.telescope-nvim" },
+  { import = "astrocommunity.media.presence-nvim" },
+  { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.fish" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+
+  -- import/override with your plugins folder
+}
