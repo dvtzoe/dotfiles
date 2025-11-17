991a303d8185867692581298514cbba4e4ecb017
dvtzoe <ohmtzoe@gmail.com>
2025-11-17 10:17:55 +0700

    config neo-tree
    uses web-devicons and shows hidden files

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
+}
