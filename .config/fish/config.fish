9b59d59c3f537adafdd4a9b830f6d888574bb202
dvtzoe <ohmtzoe@gmail.com>
2025-12-18 19:45:40 +0700

    add cargo bin to fish path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -29,5 +29,5 @@ if status is-interactive # Commands to run in interactive sessions can go here
 
     alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.local/bin $HOME/.cargo/bin
 end
